231dae089a787daad9c5482c0ab1c4d5c1443ad8
damyjy <49520432+zsqw123@users.noreply.github.com>
2020-12-22 14:19:26 +0800

    - 122

---
+++ 200521/src/0122.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println(maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
+
+}
+
+fun maxProfit(prices: IntArray): Int {
+    var profit = 0
+    if (prices.size < 2) return 0
+    for (i in 0..prices.size - 2) {
+        if (prices[i] < prices[i + 1]) profit += prices[i + 1] - prices[i]
+    }
+    return profit
+}
