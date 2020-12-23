eafb23c4f53f0ea174feec7c228ad72d7064ecf4
damyjy <49520432+zsqw123@users.noreply.github.com>
2020-12-23 23:14:22 +0800

    - 27

--- 200521/src/0027.kt
+++ 200521/src/0027.kt
@@ -1,7 +1,13 @@
-fun main(){
+fun main() {
 
 }
 
 fun removeElement(nums: IntArray, `val`: Int): Int {
-return 0
+    if (nums.isEmpty()) return 0
+    var i = 0
+    for (j in nums.indices) if (nums[j] != `val`) {
+        nums[i] = nums[j]
+        i++
+    }
+    return i
 }
