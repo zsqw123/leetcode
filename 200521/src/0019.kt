52aa0dc33fbcf828241158239b28d8b71154ca79
damyjy <49520432+zsqw123@users.noreply.github.com>
2020-12-24 22:31:59 +0800

    - 19

---
+++ 200521/src/0019.kt
@@ -0,0 +1,29 @@
+fun main() {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+
+        override fun toString(): String {
+            return `val`.toString() + next.toString()
+        }
+    }
+
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        if (head == null) return null
+        val zero = ListNode(0)
+        zero.next = head
+        var nowNode = zero
+        var targetNode: ListNode? = null
+        var i = 0
+        while (true) {
+            if (i == n) targetNode = zero
+            if (nowNode.next == null) {
+                targetNode!!.next = targetNode.next!!.next
+                return zero.next
+            } else {
+                targetNode = targetNode?.next
+                nowNode = nowNode.next!!
+            }
+            i++
+        }
+    }
+}
