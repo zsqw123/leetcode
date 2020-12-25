c12c840f98e200127efbf16413690b96bcdcb2a6
damyjy <49520432+zsqw123@users.noreply.github.com>
2020-12-25 08:36:14 +0800

    - 141

---
+++ 200521/src/0141.kt
@@ -0,0 +1,45 @@
+fun main() {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+
+        override fun toString(): String {
+            return `val`.toString() + next.toString()
+        }
+    }
+
+    /**
+     * hashSet
+     * @param head ListNode?
+     * @return Boolean
+     */
+    fun hasCycle(head: ListNode?): Boolean {
+        if (head == null) return false
+        val set = hashSetOf<ListNode>()
+        var newHead = head
+        while (newHead!!.next != null) {
+            if (set.contains(newHead.next!!)) return true
+            set.add(newHead)
+            newHead = newHead.next
+            if (newHead == null) return false
+        }
+        return false
+    }
+
+    /**
+     * 快慢指针
+     * @param head ListNode?
+     * @return Boolean
+     */
+    fun hasCycle1(head: ListNode?): Boolean {
+        if (head == null) return false
+        var slow = head
+        var fast = head.next
+        if (fast == null) return false
+        while (fast!!.next != null) {
+            slow = slow!!.next
+            fast = fast.next!!.next ?: return false
+            if (slow == fast) return true
+        }
+        return false
+    }
+}
