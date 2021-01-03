5f36bcc04fcb8d6fd01f32c5fb1b3480b74a68c0
damyjy <zsqw123@vip.qq.com>
2021-01-03 13:59:46 +0800

    - 102 use dfs

--- 200521/src/0102.kt
+++ 200521/src/0102.kt
@@ -1,7 +1,28 @@
 import utils.TreeNode
+import java.util.*
 
-fun main(){
-//    fun levelOrder(root: TreeNode?): List<List<Int>> {
-//
-//    }
+fun main() {
+    /**
+     * 层序遍历, 使用非递归 dfs
+     * @param root TreeNode?
+     * @return List<List<Int>>
+     */
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        if (root == null) return emptyList()
+        val result = ArrayList<ArrayList<Int>>()
+
+        class IndexedNode(var node: TreeNode, var depth: Int)
+
+        val stack = Stack<IndexedNode>()
+        stack.add(IndexedNode(root, 1))
+        while (!stack.isEmpty()) {
+            stack.pop().apply {
+                if (depth > result.size) result.add(arrayListOf(node.`val`))
+                else result[depth - 1].add(node.`val`)
+                node.right?.let { stack.add(IndexedNode(it, depth + 1)) }
+                node.left?.let { stack.add(IndexedNode(it, depth + 1)) }
+            }
+        }
+        return result
+    }
 }
