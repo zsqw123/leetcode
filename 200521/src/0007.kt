34e0f812a44034e13f339c9fc36ce014ad2c1573
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-02-16 22:57:15 +0800

    - add 0007

---
+++ 200521/src/0007.kt
@@ -0,0 +1,11 @@
+fun main() {
+    fun reverse(x: Int): Int {
+        var out = 0L
+        var xx = x
+        while (xx != 0) {
+            out = out * 10 + xx % 10
+            xx /= 10
+        }
+        return if (out.toInt().toLong() != out) 0 else out.toInt()
+    }
+}
