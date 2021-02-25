5575b1a0d79048bdfd1729b36abcc1e2aa5db416
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-02-25 08:46:24 +0800

    - change 0867 single line

--- 200521/src/0867.kt
+++ 200521/src/0867.kt
@@ -1,8 +1,4 @@
 fun main() {
-    fun transpose(matrix: Array<IntArray>): Array<IntArray> {
-        val res = Array(matrix[0].size) { IntArray(matrix.size) }
-        for (i in matrix[0].indices) for (j in matrix.indices)
-            res[i][j] = matrix[j][i]
-        return res
-    }
+    fun transpose(matrix: Array<IntArray>): Array<IntArray> =
+        Array(matrix[0].size) { IntArray(matrix.size) }.apply { matrix.forEachIndexed { j, v -> v.forEachIndexed { i, vv -> get(i)[j] = vv } } }
 }
