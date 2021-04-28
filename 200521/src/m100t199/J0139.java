29fa38d91c9d4ee497a1b43b47e077c8d171ee04
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-04-28 15:14:10 +0800

    - 0139_2

--- 200521/src/m100t199/J0139.java
+++ 200521/src/m100t199/J0139.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class J0139 {
     class S1{
-        // O(n^3)?? 暴力迭代 超时
+        // 暴力 dfs 超时
         String s;
         List<String> wordDict;
         public boolean helper(int start){
@@ -29,4 +29,36 @@ public class J0139 {
         }
     }
 
+    // dfs + 记忆化递归 1ms 99.73%
+    class S2 {
+        String s;
+        List<String> wordDict;
+        int[] mem;
+        public boolean helper(int start){
+            if(mem[start]==2) return true;
+            if(mem[start]==1) return false;
+            outter:
+            for(String word:wordDict){
+                if(word.isEmpty()) return true;
+                if(word.length()>s.length()-start) continue;
+
+                for(int j=0;j<word.length();j++)
+                    if(s.charAt(j+start)!=word.charAt(j)){
+                        mem[start]=1;
+                        continue outter;
+                    }
+
+                mem[start]=2;
+                if(word.length()+start==s.length()||helper(start+word.length())) return true;
+            }
+            mem[start]=1;
+            return false;
+        }
+        public boolean wordBreak(String s, List<String> wordDict) {
+            this.s=s;
+            this.wordDict=wordDict;
+            mem=new int[s.length()];
+            return helper(0);
+        }
+    }
 }
