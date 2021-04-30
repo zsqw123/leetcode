2f56b1ffcadc34788738779201a02172ebde5ec6
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-04-30 19:51:45 +0800

    - 0137

--- 200521/src/a.kt
+++ 200521/src/a.kt
@@ -1,10 +1,19 @@
 import utils.prl
+import java.io.File
 
 /**
  * This file just create for test.
  */
 
 fun main() {
-    val regex=Regex("""\{.*?}""")
-    regex.find("dawwaw{awa}adwaaw")?.value.prl()
+    val root = File("C:/a")
+    val a = File(root, "awa/b/")
+    val b = File(root, "/awa/b/")
+    val c = File(root, "awa/b")
+    val d = File(root, "/awa/b")
+    a.absolutePath.prl()
+    b.absolutePath.prl()
+    c.absolutePath.prl()
+    d.absolutePath.prl()
+    ((a == b) && (b == c) && (c == d)).prl()
 }
