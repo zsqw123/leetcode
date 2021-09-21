9a7ed2e69e88ddc112a0a03e4dbde06aa565c175
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-09-21 15:23:08 +0800

    - kotlin 强转类型 Reified 的作用

--- 200521/src/a.kt
+++ 200521/src/a.kt
@@ -3,32 +3,18 @@ import kotlin.reflect.KClass
 /**
  * This file just create for test.
  */
-val <A> List<A>.length
-    get() = size
+val map = hashMapOf<String, Any>()
+inline fun <reified T> getTReifiedNull(): T? {
+    return map[""] as? T
+}
 
-val <A> List<A>.anyone
-    get() = random()
+fun <T> getTNull(): T? {
+    return map[""] as? T
+}
 
-// 万能 List
-val nothingList get() = listOf<Nothing>()
-
-val strList: List<String> = nothingList
-val intList: List<Int> = nothingList
-
-val <T> T.singleItemList: ArrayList<T> get() = arrayListOf(this)
-val <T> T.isStr: Boolean get() = this is String
-
-// 得到集合的泛型类型
-inline val <reified T : Any> Collection<T>.itemKClass: KClass<T> get() = T::class
-
-val <T> T.printThen: T get() = also(::println)
-fun genList(): List<Number> = listOf(1)
 fun main() {
-    genList().itemKClass.printThen
-    genList()[0]::class.printThen
-    listOf(1).itemKClass.printThen
-    1.isStr.printThen
-    2.singleItemList.printThen
-    "hello".uppercase().length
-    "hello".printThen.uppercase().printThen.length.printThen
+    val r = getTReifiedNull<Int>()
+    println(r)
+    val a = getTNull<Int>()
+    println(a)
 }
