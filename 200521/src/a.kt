a0848d4215576a26d764982990941afa9af77193
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-10-10 22:31:49 +0800

    - 0025-2

--- 200521/src/a.kt
+++ 200521/src/a.kt
@@ -1,12 +1,3 @@
-/**
- * This file just create for test.
- */
-
-class User(val map: Map<String, Any?>) {
-    val name: String by map
-}
 
 fun main() {
-    val user = User(mapOf("name" to "zzz"))
-    println(user.name)
 }
--- 200521/src/m001t099/J0025.java
+++ 200521/src/m001t099/J0025.java
@@ -24,4 +24,31 @@ public class J0025 {
         }
         return dummy.next;
     }
+
+    // 2021-10-10 二刷
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        if(head==null) return null;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode pre=dummy,singlePre=dummy,tmp,start=head,now=head;
+        int depth=0;
+        while(head!=null){
+            depth++;
+            head=head.next;
+        }
+        int times=depth/k;
+        for(int i=0;i<times;i++){
+            for(int j=0;j<k;j++){
+                tmp=now.next;
+                now.next=pre;
+                pre=now;
+                now=tmp;
+            }
+            singlePre.next=pre;
+            singlePre=start;
+            start.next=now;
+            start=now;
+        }
+        return dummy.next;
+    }
 }
