00e1ed8d47daa5a738981fc2a001ec7a42247494
damyjy <49520432+zsqw123@users.noreply.github.com>
2021-10-11 11:17:15 +0800

    - jz 32-3

---
+++ 200521/src/jz/S32_3.java
@@ -0,0 +1,36 @@
+package jz;
+
+import utils.TreeNode;
+
+import java.util.*;
+
+public class S32_3 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        Deque<TreeNode> deque=new LinkedList<>();
+        List<List<Integer>> res=new ArrayList<>();
+
+        deque.offer(root);
+        boolean leftStart=false;
+        List<Integer> single=new ArrayList<>();
+        single.add(root.val);
+        res.add(single);
+        while(!deque.isEmpty()){
+            LinkedList<TreeNode> tmp=new LinkedList<>();
+            int sz=deque.size();
+            for(int i=0;i<sz;i++){
+                TreeNode now=deque.poll();
+                if(now.left!=null) tmp.offer(now.left);
+                if(now.right!=null) tmp.offer(now.right);
+            }
+            if(tmp.isEmpty()) break;
+            List<Integer> r=new ArrayList<>();
+            for(TreeNode t:tmp) r.add(t.val);
+            if(!leftStart) Collections.reverse(r);
+            res.add(r);
+            deque=tmp;
+            leftStart=!leftStart;
+        }
+        return res;
+    }
+}
